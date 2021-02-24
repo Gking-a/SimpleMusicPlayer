974932ce72469e4d2f2949952383b6c300353bbc
Gking <2665241312@qq.com>
2021-02-24 18:02:52 +0800

    update

---
+++ run/Receive.java
@@ -0,0 +1,22 @@
+import java.io.File;
+public class Receive {
+    public static void main(String[] args) throws Exception {
+        if(args==null||args.length==0)
+            args=new String[]{"45460"};
+        new Receive().main(Integer.valueOf(args[0]));
+    }
+    private void main(int port) throws Exception {
+        GLibrary config=new GLibrary(new File("/sdcard/AppProjects/SimpleMusicPlayer/run","config"));
+        config.connect();
+        config.add("port",port,GLibrary.TYPE_STRING);
+        config.add("ip",Util.getIP(),GLibrary.TYPE_STRING);
+        config.save();
+        File file = new File("/storage/emulated/0/AppProjects/apk");
+        TCPServer s=new TCPServer(port);
+        s.accept();
+        while(!s.isAccept()){
+            Thread.sleep(200);
+        }
+        s.receive(file);
+    }
+}
