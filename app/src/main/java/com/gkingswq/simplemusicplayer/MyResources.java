64d40d968271242e0a5858e933e7743fea98c928
Gking <2665241312@qq.com>
2021-03-05 12:42:04 +0800

    update

--- app/src/main/java/com/gkingswq/simplemusicplayer/MyResources.java
+++
@@ -1,9 +0,0 @@
-package com.gkingswq.simplemusicplayer;
-
-import android.view.WindowManager;
-import android.widget.RemoteViews;
-
-public strictfp final class MyResources {
-    public static RemoteViews view;
-    public static WindowManager windowManager;
-}
