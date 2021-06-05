f01d3a3385e770b0fdafacf0723d87909395a961
Gking <2665241312@qq.com>
2021-06-05 20:17:40 +0800

    update

--- app/src/main/java/com/gkingswq/simplemusicplayer/Launcher.java
+++ app/src/main/java/com/gkingswq/simplemusicplayer/Launcher.java
@@ -11,7 +11,7 @@ public class Launcher extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.launcher);
     }
     
 }
