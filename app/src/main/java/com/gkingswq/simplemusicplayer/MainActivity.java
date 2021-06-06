a07a7cdc5648b6a350aec21dd71ac26cfe2c3061
Gking <2665241312@qq.com>
2021-06-06 09:22:29 +0800

    update

--- app/src/main/java/com/gkingswq/simplemusicplayer/MainActivity.java
+++ app/src/main/java/com/gkingswq/simplemusicplayer/MainActivity.java
@@ -5,8 +5,11 @@ package com.gkingswq.simplemusicplayer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     
     public static final String TAG = "MainActivity";
     
@@ -14,6 +17,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toolbar toolbar=f(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    public <T extends View> T f(int id) {
+        return super.findViewById(id);
     }
     
 }
