3f9113fab524e71d27bf217a91621b92f16e8531
Gking <2665241312@qq.com>
2021-06-08 22:19:11 +0800

    update

--- app/src/main/java/com/gkingswq/simplemusicplayer/util/Web163.java
+++ app/src/main/java/com/gkingswq/simplemusicplayer/util/Web163.java
@@ -2,6 +2,14 @@
  */
 
 package com.gkingswq.simplemusicplayer.util;
+import java.util.HashMap;
+import java.util.List;
+import java.math.BigInteger;
+import java.util.Collections;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import android.util.Base64;
 
 public class Web163 {
     private static final String modulus = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7";
@@ -54,7 +62,7 @@ public class Web163 {
         IvParameterSpec iv = new IvParameterSpec("0102030405060708".getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
         byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
-        return new Base64().encodeToString(encrypted);//此处使用BASE64做转码。
+        return new Base64().encodeToString(encrypted,0);//此处使用BASE64做转码。
     }
     /**
      * 截取长度
