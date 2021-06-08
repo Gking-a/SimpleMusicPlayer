41a78e0895c8909fbb98961aa2460e8d2b09c3b6
Gking <2665241312@qq.com>
2021-06-08 19:31:31 +0800

    update

--- app/src/main/java/com/gkingswq/simplemusicplayer/Receiver.java
+++ app/src/main/java/com/gkingswq/simplemusicplayer/Receiver.java
@@ -8,6 +8,11 @@ import android.os.Bundle;
 import android.content.Intent;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import gtools.SingleThreadTask;
+import org.json.JSONObject;
+import com.gkingswq.simplemusicplayer.util.CopyWeb;
+import org.json.JSONException;
+import com.gkingswq.simplemusicplayer.util.FW;
 
 public class Receiver extends Activity {
     
@@ -21,11 +26,22 @@ public class Receiver extends Activity {
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             path = ti.getStringExtra(Intent.EXTRA_TEXT);
         }
+        
         //http://music.163.com/playlist/6606033030/1607939711/?userid=1607939711 
         Pattern idPattern1=Pattern.compile("/playlist/(.*?)/");
         Matcher matcher=idPattern1.matcher(path);
         if(matcher.find()){
-            String playlistId=matcher.group();
+            final String playlistId=matcher.group();
+            Thread t=new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject jsonRoot=new JSONObject(
+                                CopyWeb.doGet("https://api.imjad.cn/cloudmusic/?type=playlist&id=" + playlistId));
+                            FW.w(jsonRoot.toString());
+                        } catch (JSONException e) {}}
+                });
+            t.start();
             
         }
         
